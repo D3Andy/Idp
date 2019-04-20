79641eb42660a2d3bcf6fc32ef124609e1833a83
Andrei Stan <andrei_stan_96@yahoo.com>
2019-04-20 11:58:03 +0300

    first

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,19 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Road")
+option_b = os.getenv('OPTION_B', "Offroad")
+option_c = os.getenv('OPTION_C', "Both")
+height_a = os.getenv('HEIGHT_A', "150-170 cm")
+height_b = os.getenv('HEIGHT_B', "170-190 cm")
+height_c = os.getenv('HEIGHT_C', "190-200 cm")
+way_a = os.getenv('WAY_A', "Flat")
+way_b = os.getenv('WAY_B', "Mostly Flat")
+way_c = os.getenv('WAY_C', "Some hills")
+way_d = os.getenv('WAY_D', "Some Mountain roads")
+fit_a = os.getenv('FIT_A', "Amator")
+fit_b = os.getenv('FIT_B', "Good")
+fit_c = os.getenv('FIT_C', "Competitive")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -34,6 +45,17 @@ def hello():
         'index.html',
         option_a=option_a,
         option_b=option_b,
+	option_c=option_c,
+	height_a=height_a,
+	height_b=height_b,
+	height_c=height_c,
+	way_a=way_a,
+	way_b=way_b,
+	way_c=way_c,
+	way_d=way_d,
+	fit_a=fit_a,
+	fit_b=fit_b,
+	fit_c=fit_c,
         hostname=hostname,
         vote=vote,
     ))
